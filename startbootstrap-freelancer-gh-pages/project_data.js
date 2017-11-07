69edb1a50d3168cf744fb9a2c42ff32dc4a2e06e
Houssem Boundaoui <hbound2@uic.edu>
2017-11-07 21:37:33 +0000

    update

---
+++ startbootstrap-freelancer-gh-pages/project_data.js
@@ -0,0 +1,7 @@
+var projects = [
+	{"name": "Weather App", "display": false, "description" : "This is a progressive weather app that I created for my IT202 class.", "technologies": "AJAX, Responsive Design, Material Components", "image" : "<some image url>", etc.},
+	{"name": "Chicago Restaurant Inspection", "display" : true ,"description" : "A really cool restaurant project using AJAX, Canvas, Service Workers and funny animal sounds.", "technologies": "AJAX, Responsive Design, Material Components", "image" : "<some image url>", etc.},
+	etc
+
+
+]
