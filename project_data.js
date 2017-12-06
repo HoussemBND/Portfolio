a85fb0f15baba8aa5366a799aba08612f7d1c201
Houssem Boundaoui <hbound2@uic.edu>
2017-12-06 00:58:50 +0000

    update

---
+++ project_data.js
@@ -0,0 +1,49 @@
+var projects = [
+    //Project 1
+    {
+        "name": 'Simple "About Me" Page',
+        "link": "https://houssembnd.github.io/IT202/project1/index.html",
+        "id": "portfolioModal1",
+        "desc": "A simple web app that use basic HTML and CSS to make a profile page. Also include link to personal top 3 TV shows.",
+        "tool": "HTML, CSS",
+        "github": "https://github.com/houssembnd/IT202/tree/master/project1"
+    },
+    //Project 2
+    {
+        "name": 'Using a Bootstrap Template/Theme',
+        "link": "https://houssembnd.github.io/IT202/project2/index.html",
+        "id": "portfolioModal2",
+        "desc": "Using bootstrap template and modifying it.",
+        "tool": "Bootstrap, Bootstrap Template",
+        "github": "https://github.com/houssembnd/IT202/tree/master/project2"
+    },
+    //Project 3
+    {
+        "name": 'Data App With User Filter',
+        "link": "https://houssembnd.github.io/IT202/project3/index.html",
+        "id": "portfolioModal3",
+        "desc": "Get user input and then filter API request in order to display result in list and Google Map.",
+        "tool": "Google Map API, API, Bootstrap 4, AJAX, jQuery, JavaScript",
+        "github": "https://github.com/houssembnd/IT202/tree/master/project3"
+    },
+    //Project 4
+    {
+        "name": 'Simple Scrolling Game',
+        "link": "https://houssembnd.github.io/IT202/project4/index.html",
+        "id": "portfolioModal4",
+        "desc": "Use Canvas to make a simple scrolling game. Objective of the game is to collect green circles but avoid red circle.",
+        "tool": "Canvas, jQuery",
+        "github": "https://github.com/houssembnd/IT202/tree/master/project4"
+    },
+    //Project 5
+    {
+        "name": 'First Progressive Web App',
+        "link": "https://houssembnd.github.io/IT202/project5/",
+        "id": "portfolioModal5",
+        "desc": "Building a simple Progressive Web Application that display the weather.",
+        "tool": "PWA",
+        "picture": "project5",
+
+        "github": "https://github.com/houssembnd/IT202/tree/master/project5"
+    }
+];
